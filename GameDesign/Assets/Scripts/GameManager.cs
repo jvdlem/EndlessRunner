09a0635a841a1ca4897eedf0f8a4ac5ad9931578
Tiara Vrossink <71695366+arctantica@users.noreply.github.com>
2023-09-20 15:17:02 +0200

    basic game manager

---
+++ GameDesign/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum gameStates
+    {
+        MainMenu,
+        Playing,
+        GameOver
+    }
+    public gameStates currentState;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case gameStates.MainMenu:
+                break;
+
+            case gameStates.Playing:
+                break;
+
+            case gameStates.GameOver:
+                break;
+        }
+    }
+}
